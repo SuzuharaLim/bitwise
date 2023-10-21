bfd0ca3de87d20da618463f00157437c4bf4be7e
SuzuharaLim <suzuharalim@gmail.com>
2023-10-21 08:37:13 +0800

    updata

---
+++ bitwise/bitwise.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int add(int a, int b) {
+    while (b != 0) {
+        int carry = a & b;
+        a = a ^ b;
+        b = carry << 1;
+    }
+    return a;
+}
+
+int subtract(int a, int b) {
+    b = add(~b, 1);
+    return add(a, b);
+}
+
+int main() {
+    cout << add(2, 3)<<endl;
+    cout << subtract(2,3) << endl;
+}
